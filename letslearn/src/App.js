e25c5894f9828c33fa825a6280f3dd6805c25131
Harish-ragav17 <harishragavendarraja@gmail.com>
2023-07-26 22:05:04 +0530

    Added chat component

--- letslearn/src/App.js
+++ letslearn/src/App.js
@@ -1,17 +1,18 @@
-import { Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import Quiz from './components/Quiz';
-import Protut from './components/Protut';
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Quiz from "./components/Quiz";
+import Protut from "./components/Protut";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Header/>
+        <Header />
         <Home/>
         <Footer/>
       </main>
