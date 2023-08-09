6f26a911813908d3599dfc8ebb99f017f664f83b
donal-20million <donaljoseph8@gmail.com>
2023-08-09 22:35:17 +0530

    ok

--- letslearn/src/App.js
+++ letslearn/src/App.js
@@ -1,12 +1,13 @@
-import { Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Java from './components/Java'
-import Footer from './components/Footer';
-import Quiz from './components/Quiz';
-import Protut from './components/Protut';
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Quiz from "./components/Quiz";
+import Protut from "./components/Protut";
+import Chat from "./components/Chat";
+import Python from "./components/Python";
 
 function App() {
   return (
