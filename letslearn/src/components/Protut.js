7ea0fb56e74e10b849245e26671ec868da89ba7c
ashwin <ashwinksrct@gmail.com>
2023-08-11 18:08:25 +0530

    update

--- letslearn/src/components/Protut.js
+++ letslearn/src/components/Protut.js
@@ -16,7 +16,7 @@ const Protut = () => {
   <p>C++ is one of the world's most popular programming languages. C++ can be found in today's operating systems, Graphical User Interfaces, and embedded systems.C++ is an object-oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development costs.</p>
   <div>
     <div class="btn">
-     <a>Explore &#x2192;</a>
+     <a href='#'>Explore &#x2192;</a>
     </div>
   </div>
 </div>
@@ -26,7 +26,7 @@ const Protut = () => {
   <p>Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. Python is a general-purpose language, meaning it can be used to create a variety of different programs and isn't specialized for any specific problems. Its emphasizes code readability</p>
   <div>
     <div class="btn">
-     <a>Explore &#x2192;</a>
+     <a href='#'>Explore &#x2192;</a>
     </div>
   </div>
 </div>
@@ -36,7 +36,7 @@ const Protut = () => {
   <p>Java is a widely-used programming language for coding web applications. It has been a popular choice among developers for over two decades, with millions of Java applications in use today. Java is a multi-platform, object-oriented, and network-centric language that can be used as a platform in itself.</p>
   <div>
     <div class="btn">
-     <a>Explore &#x2192;</a>
+     <a href='#'>Explore &#x2192;</a>
     </div>
   </div>
 </div>
