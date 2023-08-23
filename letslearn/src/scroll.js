034773e0b5bb53cf63e3d3d5bece96d87db54420
HARISH RAGAVENDAR <113747322+Harish-ragav17@users.noreply.github.com>
2023-08-23 13:51:28 +0530

    deleted scroll.js

--- letslearn/src/scroll.js
+++
@@ -1,9 +0,0 @@
-// window.onscroll = function() {scrollFunction()};
-// let height=85;
-// function scrollFunction() {
-//   if (document.body.scrollTop > height || document.documentElement.scrollTop > height) {
-//     document.getElementsByClassName("side-div-container").style.top="0px";
-//   } else {
-//     document.getElementsByClassName("side-div-container").style.top="69px";
-//   }
-// } 
